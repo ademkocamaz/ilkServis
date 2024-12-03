22985aa220b8ebfd6078d1f1140cdc1931db667f
Adem KOCAMAZ <kocamazadem@gmail.com>
2024-12-03 10:17:11 +0300

    Update
    - Baton Theme Admin Page unregistered.
    - Python Packages updated.

--- Source/django/service/admin.py
+++ Source/django/service/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from baton.models import BatonTheme
 
 # Register your models here.
 from service._admin.service import ServiceAdmin
+
+admin.site.unregister(BatonTheme)
